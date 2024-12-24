cfa169f2d5e8ad3087ae69e642078a328655129e
santiruizmazzo <69594002+santiruizmazzo@users.noreply.github.com>
2024-12-23 23:44:42 -0300

    Agregados todos los archivos del proyecto original

---
+++ scripts.js
@@ -0,0 +1,7 @@
+window.onscroll = function() {scrollFunction()};
+        function scrollFunction() 
+        {if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {document.getElementById("flecha").style.display = "block";}
+        else {document.getElementById("flecha").style.display = "none";}};
+
+
+            
